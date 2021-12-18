340933719844db3db43f0f10f3344c2ee0b12931
kiang <kiange@gmail.com>
2021-12-18 21:58:36 +0800

    convert svg to json

---
+++ scripts/01_svg2json.php
@@ -0,0 +1,383 @@
+<?php
+$id2meta = [
+    5 => [
+        'seat' => 51,
+        'name' => '許至樁',
+        'party' => '國',
+        'status' => 3
+    ],
+    7 => [
+        'seat' => 41,
+        'name' => '呂維胤',
+        'party' => '民',
+        'status' => 3
+    ],
+    9 => [
+        'seat' => 31,
+        'name' => '林阳乙',
+        'party' => '無',
+        'status' => 2
+    ],
+    11 => [
+        'seat' => 21,
+        'name' => '李文俊',
+        'party' => '國',
+        'status' => 2
+    ],
+    13 => [
+        'seat' => 11,
+        'name' => '劉米山',
+        'party' => '民',
+        'status' => 3
+    ],
+    15 => [
+        'seat' => 1,
+        'name' => '郭信良',
+        'party' => '無',
+        'status' => 0
+    ],
+    17 => [
+        'seat' => 52,
+        'name' => '張世賢',
+        'party' => '國',
+        'status' => 2
+    ],
+    19 => [
+        'seat' => 42,
+        'name' => '林志展',
+        'party' => '民',
+        'status' => 3
+    ],
+    21 => [
+        'seat' => 32,
+        'name' => '周奕齊',
+        'party' => '無',
+        'status' => 2
+    ],
+    23 => [
+        'seat' => 22,
+        'name' => '曾培雅',
+        'party' => '國',
+        'status' => 3
+    ],
+    25 => [
+        'seat' => 12,
+        'name' => '蔡蘇秋金',
+        'party' => '民',
+        'status' => 2
+    ],
+    27 => [
+        'seat' => 2,
+        'name' => '林炳利',
+        'party' => '無',
+        'status' => 1
+    ],
+    29 => [
+        'seat' => 53,
+        'name' => '方一峰',
+        'party' => '國',
+        'status' => 2
+    ],
+    31 => [
+        'seat' => 43,
+        'name' => '鄭佳欣',
+        'party' => '民',
+        'status' => 3
+    ],
+    33 => [
+        'seat' => 33,
+        'name' => '林志聰',
+        'party' => '民',
+        'status' => 3
+    ],
+    35 => [
+        'seat' => 23,
+        'name' => '林美燕',
+        'party' => '國',
+        'status' => 2
+    ],
+    37 => [
+        'seat' => 13,
+        'name' => '陳昆和',
+        'party' => '無',
+        'status' => 2
+    ],
+    39 => [
+        'seat' => 3,
+        'name' => '趙昆原',
+        'party' => '無',
+        'status' => 2
+    ],
+    41 => [
+        'seat' => 54,
+        'name' => '蔡育輝',
+        'party' => '國',
+        'status' => 2
+    ],
+    43 => [
+        'seat' => 44,
+        'name' => '蔡旺詮',
+        'party' => '民',
+        'status' => 3
+    ],
+    45 => [
+        'seat' => 34,
+        'name' => '陳怡珍',
+        'party' => '民',
+        'status' => 3
+    ],
+    47 => [
+        'seat' => 24,
+        'name' => '林燕祝',
+        'party' => '國',
+        'status' => 3
+    ],
+    49 => [
+        'seat' => 14,
+        'name' => '郭清華',
+        'party' => '民',
+        'status' => 2
+    ],
+    51 => [
+        'seat' => 4,
+        'name' => '谷暮·哈就',
+        'party' => '無',
+        'status' => 2
+    ],
+    53 => [
+        'seat' => 55,
+        'name' => '蔡淑惠',
+        'party' => '國',
+        'status' => 2
+    ],
+    55 => [
+        'seat' => 45,
+        'name' => '吳禹寰',
+        'party' => '無',
+        'status' => 2
+    ],
+    57 => [
+        'seat' => 35,
+        'name' => '王錦德',
+        'party' => '民',
+        'status' => 2
+    ],
+    59 => [
+        'seat' => 25,
+        'name' => '杜素吟',
+        'party' => '無',
+        'status' => 2
+    ],
+    61 => [
+        'seat' => 15,
+        'name' => '蔡秋蘭',
+        'party' => '無',
+        'status' => 2
+    ],
+    63 => [
+        'seat' => 5,
+        'name' => '盧崑福',
+        'party' => '國',
+        'status' => 1
+    ],
+    65 => [
+        'seat' => 56,
+        'name' => '尤榮智',
+        'party' => '國',
+        'status' => 2
+    ],
+    67 => [
+        'seat' => 46,
+        'name' => '吳通龍',
+        'party' => '民',
+        'status' => 2
+    ],
+    71 => [
+        'seat' => 26,
+        'name' => '黃麗招',
+        'party' => '無',
+        'status' => 3
+    ],
+    73 => [
+        'seat' => 16,
+        'name' => '李啟維',
+        'party' => '民',
+        'status' => 3
+    ],
+    75 => [
+        'seat' => 6,
+        'name' => '曾信凱',
+        'party' => '無',
+        'status' => 2
+    ],
+    77 => [
+        'seat' => 57,
+        'name' => '洪玉鳳',
+        'party' => '國',
+        'status' => 2
+    ],
+    79 => [
+        'seat' => 47,
+        'name' => '周麗津',
+        'party' => '民',
+        'status' => 3
+    ],
+    81 => [
+        'seat' => 37,
+        'name' => '楊中成',
+        'party' => '台',
+        'status' => 3
+    ],
+    83 => [
+        'seat' => 27,
+        'name' => '邱莉莉',
+        'party' => '民',
+        'status' => 3
+    ],
+    85 => [
+        'seat' => 17,
+        'name' => '郭鴻儀',
+        'party' => '民',
+        'status' => 3
+    ],
+    87 => [
+        'seat' => 7,
+        'name' => '林易瑩',
+        'party' => '時',
+        'status' => 3
+    ],
+    91 => [
+        'seat' => 48,
+        'name' => '李中岑',
+        'party' => '國',
+        'status' => 2
+    ],
+    93 => [
+        'seat' => 38,
+        'name' => '謝財旺',
+        'party' => '無',
+        'status' => 1
+    ],
+    95 => [
+        'seat' => 28,
+        'name' => '李偉智',
+        'party' => '民',
+        'status' => 3
+    ],
+    97 => [
+        'seat' => 18,
+        'name' => '謝龍介',
+        'party' => '國',
+        'status' => 2
+    ],
+    99 => [
+        'seat' => 8,
+        'name' => '許又仁',
+        'party' => '無',
+        'status' => 2
+    ],
+    103 => [
+        'seat' => 49,
+        'name' => '李宗翰',
+        'party' => '民',
+        'status' => 3
+    ],
+    105 => [
+        'seat' => 39,
+        'name' => '陳秋宏',
+        'party' => '民',
+        'status' => 3
+    ],
+    107 => [
+        'seat' => 29,
+        'name' => '穎艾達利',
+        'party' => '無',
+        'status' => 3
+    ],
+    109 => [
+        'seat' => 19,
+        'name' => '王家貞',
+        'party' => '國',
+        'status' => 2
+    ],
+    111 => [
+        'seat' => 9,
+        'name' => '陳碧玉',
+        'party' => '民',
+        'status' => 3
+    ],
+    115 => [
+        'seat' => 50,
+        'name' => '蔡筱薇',
+        'party' => '民',
+        'status' => 3
+    ],
+    117 => [
+        'seat' => 40,
+        'name' => '沈家鳳',
+        'party' => '民',
+        'status' => 3
+    ],
+    119 => [
+        'seat' => 30,
+        'name' => '李鎮國',
+        'party' => '國',
+        'status' => 2
+    ],
+    121 => [
+        'seat' => 20,
+        'name' => '沈震東',
+        'party' => '民',
+        'status' => 3
+    ],
+    123 => [
+        'seat' => 10,
+        'name' => '陳秋萍',
+        'party' => '民',
+        'status' => 3
+    ],
+];
+$color2status = [
+    'd3d3d3' => 1, //未簽到
+    'ffffff' => 2, //簽到且出席
+    'ffbdbe' => 3, //簽到未出席
+];
+$basePath = dirname(__DIR__);
+
+function cmp($a, $b)
+{
+    if ($a['seat'] == $b['seat']) {
+        return 0;
+    }
+    return ($a['seat'] < $b['seat']) ? -1 : 1;
+}
+
+foreach (glob($basePath . '/svg/*.svg') as $svgFile) {
+    $result = [
+        'statusColor' => [
+            'empty' => '#fff',
+            '1' => '#B2B2B2',
+            '2' => '#FFF',
+            '3' => '#FFD7D7',
+        ],
+        'seats' => [],
+    ];
+    $p = pathinfo($svgFile);
+    $svg = file_get_contents($svgFile);
+    $pos = strpos($svg, '<rect');
+    $count = 0;
+    while (false !== $pos) {
+        $posEnd = strpos($svg, '/>', $pos);
+        $parts = explode('"', substr($svg, $pos, $posEnd - $pos));
+        $id = substr($parts[3], 4);
+        if (isset($id2meta[$id])) {
+            $color = substr($parts[1], 6, 6);
+            $id2meta[$id]['status'] = $color2status[$color];
+            $result['seats'][] = $id2meta[$id];
+        }
+        $pos = strpos($svg, '<rect', $posEnd);
+    }
+    usort($result['seats'], 'cmp');
+    file_put_contents($basePath . '/json/' . $p['filename'] . '.json', json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
+
